7a7c291110b873fe86ea8fcb6ad19a9c769a405b
Chomtana <Chomtana001@gmail.com>
2022-08-19 10:17:07 +0700

    Fix stabilizer

--- stabilizer.sh
+++ stabilizer.sh
@@ -1,5 +1,5 @@
 subdomain=$(cat /home/pi/localtunnel-healthcheck/subdomain)
-status_code=$(curl --write-out %{http_code} --silent --output /dev/null $subdomain/healthcheck)
+status_code=$(curl --write-out %{http_code} --silent --output /dev/null http://$subdomain.lt.airin1.com/healthcheck)
 
 if [[ "$status_code" -ne 200 ]] ; then
   pm2 restart nodered-localtunnel
